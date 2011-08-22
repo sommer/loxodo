27fb5558904bdadef1d65ea8b5b2c37526e521b4
Adam Hamsik <adam.hamsik@chillisys.com>
2011-08-22 14:44:19 +0200

    Add first example of working loxodo web interface.

--- Loxodo/frontends/web/loxodo.py
+++ Loxodo/frontends/web/loxodo.py
@@ -11,7 +11,7 @@ from ...db.vault import Vault
 from ...config import config
 from flask import Flask, session, redirect, url_for, escape, request
 
-DB_PATH="/tmp/test4.db"
+DB_PATH="/tmp/test-v41.db"
 DB_FORMAT="v4"
 
 class Webloxodo:
@@ -31,15 +31,37 @@ def index():
         return 'Logged in as %s' % escape(session['logged_in'])
     return 'You are not authenticated to vault'
 
+@app.route('/list')
+def list():
+  if 'logged_in' in session:
+    vault_records = webloxo.vault.records[:]
+    print ""
+    print "[group.title] username"
+    print "URL: url"
+    print "Notes: notes"
+    print "Last mod: modification time"
+    print "-"*10
+    for record in vault_records:
+        print "[%s.%s] %s" % (record.group.encode('utf-8', 'replace'),
+                               record.title.encode('utf-8', 'replace'),
+                               record.user.encode('utf-8', 'replace'))
+        if self.output == 'verbose':
+          if record.url:
+              print "    URL: %s" % (record.url.encode('utf-8', 'replace'))
+          if record.notes:
+              print "    Notes: %s" % (record.notes.encode('utf-8', 'replace'))
+          if record.last_mod != 0:
+              print "    Last mod: %s" % time.strftime('%Y/%m/%d',time.gmtime(record.last_mod))
+    print "-"*15
+    print ""
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if 'logged_in' in session:
       return 'You have been authenticated to vault already.'
     if request.method == 'POST':
-        webloxo.vault = Vault(request.form['password'], filename=webloxo.vault_file, format=webloxo.vault_format)
-        print request.form['password']
-        print webloxo.vault 
-        print webloxo.vault_file
+        webloxo.password=request.form['password'].encode('utf-8','replace')
+        webloxo.vault = Vault(webloxo.password, filename=webloxo.vault_file, format=webloxo.vault_format)
         if webloxo.vault != None:
           session['logged_in'] = request.form['password']
         return redirect(url_for('index'))
@@ -53,12 +75,12 @@ def login():
 @app.route('/logout')
 def logout():
     # remove the username from the session if its there
-    session.pop('username', None)
+    session.pop('logged_in', None)
     return redirect(url_for('index'))
 
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == "Loxodo.frontends.web.loxodo":
-    app.debug = True
+    app.debug = False
     app.run()
