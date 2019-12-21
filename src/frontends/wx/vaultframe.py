4825181c7edacc382f886b459bf22b70b83616a4
Jason Ansel <jansel@jansel.net>
2019-12-21 11:09:21 -0800

    Use icon for main frame

--- src/frontends/wx/vaultframe.py
+++ src/frontends/wx/vaultframe.py
@@ -225,6 +225,11 @@ class VaultFrame(wx.Frame):
 
         self._searchbox.SetFocus()
 
+        self.icon = wx.Icon(os.path.join(os.path.dirname(os.path.realpath(config.get_basescript())),
+                                         "resources", "loxodo-icon.png"),
+                            wx.BITMAP_TYPE_PNG, 128, 128)
+        self.SetIcon(self.icon)
+
         self.vault_file_name = None
         self.vault_password = None
         self.vault = None
